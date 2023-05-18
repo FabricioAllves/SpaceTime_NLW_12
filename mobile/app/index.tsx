a5d92ac0442e6d18d2191529532c21fba7d7f83a
fabricio-26 <fabricioohh@gmail.com>
2023-05-18 17:24:55 -0300

    Finalizaçao do  Mobile - 3ºDia

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,16 +1,19 @@
 import { StatusBar } from 'expo-status-bar'
+import { useEffect } from 'react'
+import { useRouter } from "expo-router";
 import { ImageBackground, Text, TouchableOpacity, View } from 'react-native'
+import * as SecureStore from 'expo-secure-store'
 import {makeRedirectUri, useAuthRequest} from 'expo-auth-session'
 
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
 import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
 
-import blurBg from './src/assets/bg-blur.png'
-import Stripes from './src/assets/stripes.svg'
-import NlwLogo from './src/assets/nlw-spacetime-logo.svg'
+
+import blurBg from '../src/assets/bg-blur.png'
+import Stripes from '../src/assets/stripes.svg'
+import NlwLogo from '../src/assets/nlw-spacetime-logo.svg'
 import { styled } from 'nativewind'
-import { useEffect } from 'react'
-import { api } from './src/libs/apis'
+import { api } from '../src/libs/apis'
 
 const StyledStripes = styled(Stripes)
 
@@ -21,6 +24,8 @@ const discovery = {
 };
 
 export default function App() {
+  const router = useRouter()
+
   const [loadedFonts] = useFonts({
     Roboto_400Regular,
     Roboto_700Bold,
@@ -38,17 +43,23 @@ export default function App() {
     discovery
   );
 
+  async function handleGithubOAuthCode(code: string) {
+    const response = await api.post('/register', {
+      code,
+    })
+
+    const {token} = response.data
+
+    await SecureStore.setItemAsync('token', token)
+
+    router.push('/memories')
+  }
+
   useEffect(() => {
     if (response?.type === 'success') {
       const { code } = response.params;
       
-      api.post('/register', {
-        code,
-      }).then((response) => {
-        const {token} = response.data
-
-        console.log(token)
-      })
+      handleGithubOAuthCode(code)
     }
   }, [response]);
 
