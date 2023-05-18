999c224a487b96dac5f8282b0315670005845ba9
fabricio henrique alves <fabricioohh@gmail.com>
2023-05-18 07:25:03 -0300

    3º Dia

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar'
 import { ImageBackground, Text, TouchableOpacity, View } from 'react-native'
+import {makeRedirectUri, useAuthRequest} from 'expo-auth-session'
 
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
 import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
@@ -8,8 +9,16 @@ import blurBg from './src/assets/bg-blur.png'
 import Stripes from './src/assets/stripes.svg'
 import NlwLogo from './src/assets/nlw-spacetime-logo.svg'
 import { styled } from 'nativewind'
+import { useEffect } from 'react'
+import { api } from './src/libs/apis'
 
 const StyledStripes = styled(Stripes)
+
+const discovery = {
+  authorizationEndpoint: 'https://github.com/login/oauth/authorize',
+  tokenEndpoint: 'https://github.com/login/oauth/access_token',
+  revocationEndpoint: 'https://github.com/settings/connections/applications/7de6306d2f1991c9151a',
+};
 
 export default function App() {
   const [loadedFonts] = useFonts({
@@ -17,6 +26,31 @@ export default function App() {
     Roboto_700Bold,
     BaiJamjuree_700Bold
   })
+
+  const [request, response, signInWithGithub] = useAuthRequest(
+    {
+      clientId: '7de6306d2f1991c9151a',
+      scopes: ['identity'],
+      redirectUri: makeRedirectUri({
+        scheme: 'nlwspacetime',
+      }),
+    },
+    discovery
+  );
+
+  useEffect(() => {
+    if (response?.type === 'success') {
+      const { code } = response.params;
+      
+      api.post('/register', {
+        code,
+      }).then((response) => {
+        const {token} = response.data
+
+        console.log(token)
+      })
+    }
+  }, [response]);
 
   if (!loadedFonts) {
     return null
@@ -40,6 +74,7 @@ export default function App() {
         </View>
         <TouchableOpacity 
         activeOpacity={0.7}
+        onPress={() => signInWithGithub()}
         className='rounded-full bg-green-500 px-5 py-2'>
           <Text className='font-alt text-sm uppercase text-black'>Cadastar lembrança</Text>
         </TouchableOpacity>
